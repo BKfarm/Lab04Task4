5c9e3794215190ee13e3d9111579292cad1963c3
Ben Fahrmeier <144169391+BKfarm@users.noreply.github.com>
2023-09-15 21:05:28 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 2;
+        System.out.println("Number: " + numToExamine);
+        if (numToExamine % 2 == 0)
+        {
+            System.out.println("Number is Even!");
+        } else 
+        {
+            System.out.println("Number is Odd!");
+        }
+        numToExamine = 3;
+        System.out.println("Number: " + numToExamine);
+        if (numToExamine % 2 == 0)
+        {
+            System.out.println("Number is Even!");
+        } else 
+        {
+            System.out.println("Number is Odd!");
+        }
+    }
+}
